3a3cb5801890ec362138deee744e480635b976cb
ikepon <tatsuyanoheya@gmail.com>
2016-05-20 15:32:59 +0900

    セミコロンがない箇所に追加

--- app/routes/programmers.js
+++ app/routes/programmers.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['Ada Lovelace', 'Edward Snowden', 'Julian Assange']
+    return ['Ada Lovelace', 'Edward Snowden', 'Julian Assange'];
   }
 });
